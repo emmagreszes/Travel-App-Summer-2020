967b39533c3c9c9c22f85f96eea03e2a6955d0d0
emmagreszes <49723537+emmagreszes@users.noreply.github.com>
2020-06-04 20:01:50 -0400

    photos

--- Files/assets/data.js
+++ Files/assets/data.js
@@ -3,67 +3,60 @@ export default
   {
    "id": 1,
     "City": "Waltham",
-
     "Country": "USA",
-
     "img": {
-      "src": "media/antonella.jpg",
-      "alt": "Antonella DiLillo"
+      "src": "media/waltham.jpg",
+      "alt": "Waltham"
     },
   },
+
   {
   "id": 2,
    "City": "Jerusalem",
-
    "Country": "Israel",
-
    "img": {
-     "src": "media/antonella.jpg",
-     "alt": "Antonella DiLillo"
+     "src": "media/jerusalem.jpg",
+     "alt": "Jerusalem"
    }
  },
+
  {
  "id": 3,
   "City": "Baltimore",
-
   "Country": "USA",
-
   "img": {
-    "src": "media/antonella.jpg",
-    "alt": "Antonella DiLillo"
+    "src": "media/baltimore.jpg",
+    "alt": "Baltimore"
   },
 },
+
 {
   "id": 4,
    "City": "Teaneck",
-
    "Country": "USA",
-
    "img": {
-     "src": "media/antonella.jpg",
-     "alt": "Antonella DiLillo"
+     "src": "media/teaneck.jpg",
+     "alt": "Teaneck"
    },
 },
+
 {
   "id": 5,
    "City": "Manhattan",
-
    "Country": "USA",
-
    "img": {
-     "src": "media/antonella.jpg",
-     "alt": "Antonella DiLillo"
+     "src": "media/manhattan.jpg",
+     "alt": "Manhattan"
    },
 },
+
 {
   "id": 6,
    "City": "Amsterdam",
-
    "Country": "Netherlands",
-
    "img": {
-     "src": "media/antonella.jpg",
-     "alt": "Antonella DiLillo"
+     "src": "media/amsterdam.jpg",
+     "alt": "Amsterdam"
    },
 },
 ]
